a8f153b60bbce5a748ddf8d737009724633f14ad
JinieOH <hijinie20@gmail.com>
2020-07-10 13:16:09 +0900

    Posts 구성

---
+++ src/main/java/com/luce/carrotmarket/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.luce.carrotmarket.web.dto;
+
+import com.luce.carrotmarket.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime chgDT;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.chgDT = entity.getChgDT();
+    }
+}
